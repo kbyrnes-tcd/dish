e3967a682a2617e0d4436be2babc41ab82faa77d
zachsmithgraphics-rgb <zachsmithgraphics@gmail.com>
2026-03-12 14:02:59 +0000

    added files: app.js, assets/style.css, index.html
    general structure with no content yet
    
    app.js runs a simple server that serves the index.html file and the style.css file. The index.html file contains the basic structure of the webpage, and the style.css file contains the styling for the webpage.

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path")
+const PORT = 8000;
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "./assets")));
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/index.html"));
+});
+
+app.listen( PORT, () => {
+    console.log(`App running on http://localhost:${PORT}`);
+})
