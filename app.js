6309b555aa48ccf208551e31aee5c02c792f54ae
zachsmithgraphics-rgb <zachsmithgraphics@gmail.com>
2026-03-12 15:53:38 +0000

    Working on adding mysql database into project files

--- app.js
+++ app.js
@@ -16,3 +16,11 @@ app.listen( PORT, () => {
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "./views"));
+
+require("dotenv").config();
+const DBCONFIG = {
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
+}
