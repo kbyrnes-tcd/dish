bab469b88267a18a4053229a3d00ca7b495d4809
zachsmithgraphics-rgb <zachsmithgraphics@gmail.com>
2026-03-28 21:20:31 +0000

    adjusted sql file with updated tables

--- dish.sql
+++ dish.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Mar 25, 2026 at 08:55 PM
+-- Generation Time: Mar 28, 2026 at 09:19 PM
 -- Server version: 8.0.44
 -- PHP Version: 8.3.30
 
@@ -28,13 +28,64 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `dishes` (
-  `id` int UNSIGNED NOT NULL,
-  `restaurant_id` int UNSIGNED NOT NULL,
-  `dish_name` varchar(50) NOT NULL,
-  `course_type` varchar(50) NOT NULL,
-  `dish_cuisine` varchar(50) NOT NULL,
-  `dish_description` text
+  `id` int NOT NULL,
+  `dish_name` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `dishes`
+--
+
+INSERT INTO `dishes` (`id`, `dish_name`) VALUES
+(1, 'Crispy Golden Chicken Tenders'),
+(2, 'Boneless Wings'),
+(3, 'Buffalo Wings'),
+(4, 'The Classic'),
+(5, 'Cheeeze Pleeze'),
+(6, 'Jalapeno Popper'),
+(7, 'Smokestack'),
+(8, 'Bacon Cheeeze'),
+(9, 'Supernova'),
+(10, 'The M50'),
+(11, 'Beyond Meat Truffle'),
+(12, 'Rocket Dog'),
+(13, 'Bacon Big Boy'),
+(14, 'Caesar Salad'),
+(15, 'Cheeezy Nachos'),
+(16, 'Kitchen Sink Nachos'),
+(17, 'Regular Fries'),
+(18, 'Rocket Rings'),
+(19, 'Buffalo Fries'),
+(20, 'Waffle Chili Cheeze Fries'),
+(21, 'Waffle Fries'),
+(22, 'Bacon and Chese Fries'),
+(23, 'Chili Cheese Fries'),
+(24, 'Garlic and Chese Fries'),
+(25, 'Garlic Fries'),
+(26, 'Cheese Fries'),
+(27, 'Hamburger'),
+(28, 'Cheeseburger'),
+(29, 'Bacon Burger'),
+(30, 'Bacon Cheeseburger'),
+(31, 'Little Hamburger'),
+(32, 'Little Cheseburger'),
+(33, 'Little Bacon Cheseburger'),
+(34, 'Hotdog'),
+(35, 'Cheese Dog'),
+(36, 'Bacon Dog'),
+(37, 'Bacon Cheese Dog'),
+(38, 'Veggie Sandwich'),
+(39, 'Chese Veggie Sandwich'),
+(40, 'Grilled Cheese'),
+(41, 'BLT'),
+(42, 'Lettuce Wrap'),
+(43, 'Burger Bowl'),
+(44, 'Little Five Guys Style Fries'),
+(45, 'Five Guys Style Fries'),
+(46, 'Large Five Guys Style Fries'),
+(47, 'Little Cajun Fries'),
+(48, 'Cajun Fries'),
+(49, 'Large Cajun Fries');
 
 -- --------------------------------------------------------
 
@@ -81,7 +132,7 @@ INSERT INTO `restaurants` (`id`, `restaurant_name`, `restaurant_location`, `rest
 (8, 'The Winding Stair', '40 Ormond Quay Lower, North City, Dublin 1, D01 R9Y5', '€20-€30', 'The Winding Stair Bookshop & Café became a famous Dublin landmark in the 1970s and 1980s. Named after the Yeats poem and in honour of its winding staircase, it is overlooking the river Liffey, with an iconic view of the Ha\'penny bridge. As a popular meeting place for writers, musicians and artists, it was a well known hub for debate and creativity with many poems written, novels penned and movies shot within its walls.', '2026-03-24 18:29:32', 'Irish'),
 (9, 'Taco Taco at East Side Tavern', '104-105 Leeson Street Lower, Dublin, Co. Dublin D02 K710', '€20-€30', 'Taco Taco is a contemporary Mexican restaurant Dublin offering a unique twist on Mexican cuisine. Our tacos are now legendary and a must for lovers of Mexican Food. We also have an extensive cocktail list. Be sure to try our famous Margarita, it’s the best in Dublin.', '2026-03-24 18:42:40', 'Mexican'),
 (10, 'Bunsen', '36 Wexford St, Portobello, Dublin 2, D02 PW56', '€10-€20', 'Compact eatery with an equally compact menu of burgers with extra toppings and a choice of fries.', '2026-03-25 10:12:42', 'American'),
-(11, 'Eddie Rocket\'s', 'Jervis St, Dublin 1, D01 F2V0', '€10-€20', 'Colourful chain diner dishing up U.S. comfort-food classics such as burgers, fries & shakes.', '2026-03-25 10:34:55', 'American'),
+(11, 'Eddie Rockets', 'Jervis St, Dublin 1, D01 F2V0', '€10-€20', 'Colourful chain diner dishing up U.S. comfort-food classics such as burgers, fries & shakes.', '2026-03-25 10:34:55', 'American'),
 (12, 'Five Guys', '56 South Great George\'s Street, Dublin 2, D02 HF50', '€10-€20', 'Fast-food chain with made-to-order burgers, fries & hot dogs, plus free peanuts while you wait.', '2026-03-25 10:45:17', 'American'),
 (13, 'Le Bon Crubeen', '81-82 Talbot St, North City, Dublin 1, D01 CD34', '€30-€40', 'Stylish and casual wood filled dining room for European influenced food and freshly made cocktails.', '2026-03-25 10:50:49', 'French'),
 (14, 'L\'Ecrivain Restaurant', '109A Baggot Street Lower, Dublin 2, D02 V580', '€50+', 'Seasonal Irish ingredients given full French gastronomic treatment on a la carte and tasting menus.', '2026-03-25 10:56:39', 'French'),
@@ -91,6 +142,30 @@ INSERT INTO `restaurants` (`id`, `restaurant_name`, `restaurant_location`, `rest
 (18, 'Golden Dragon', 'Woodfarm, Dublin, D20 T294', '€10-€20', 'At Golden Dragon, we take pride in serving delicious, authentic Chinese cuisine made with fresh ingredients and bold flavours. Our goal is to provide every customer with a satisfying dining experience, whether you\'re enjoying a quick takeaway or a relaxed meal with us.', '2026-03-25 12:07:40', 'Chinese'),
 (19, 'China Sichuan Restaurant', 'Sandyford Business Park, Dublin 18, D18 XH28', '€20-€30', 'The China Sichuan Restaurant in Sandyford, Dublin has a long tradition of innovation and invention. We pride ourselves on delivering unique culinary experiences to our guests by bringing together the finest local ingredients and traditional Sichuan and Cantonese cookery. Our chefs take pride in serving dishes that are inspired by tradition and motivated by invention.', '2026-03-25 12:15:03', 'Chinese'),
 (20, 'The Olive Tree', 'Belgard Rd, Tallaght, Dublin 24', '€20-€30', 'The Olive Tree restaurant, located on the first floor of the Plaza Hotel in Tallaght, Dublin 24, offers Italian and European cuisine with views of the Dublin Mountains.', '2026-03-25 12:18:54', 'Greek');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `restaurants_dishes`
+--
+
+CREATE TABLE `restaurants_dishes` (
+  `id` int UNSIGNED NOT NULL,
+  `restaurant_id` int UNSIGNED NOT NULL,
+  `dishes_id` int DEFAULT NULL,
+  `course_type` varchar(50) NOT NULL,
+  `description` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `restaurants_dishes`
+--
+
+INSERT INTO `restaurants_dishes` (`id`, `restaurant_id`, `dishes_id`, `course_type`, `description`) VALUES
+(1, 11, 1, 'Entree', 'Six pieces of chicken tenders and two dunking sauces'),
+(2, 11, 2, 'Entree', 'Ten boneless wings, one dunking sauce with celery '),
+(3, 12, 30, 'Entree', 'Fresh patties hot off the grill with American-style cheese and crispy Applewood smoked bacon. Placed on a soft, toasted sesame seed bun. Add as many toppings as you want.'),
+(4, 12, 34, 'Entree', 'A simple all-beef dog, grilled to caramelized perfection. Fix it up with any of your favorite toppings.');
 
 -- --------------------------------------------------------
 
@@ -122,6 +197,13 @@ CREATE TABLE `users` (
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `user_email`, `user_password`, `user_xp`, `user_level`, `created_at`) VALUES
+(1, 'TestUser', 'testuser@gmail.com', 'password', 0, 1, '2026-03-28 21:18:17');
+
 -- --------------------------------------------------------
 
 --
@@ -145,8 +227,7 @@ CREATE TABLE `user_dishes` (
 -- Indexes for table `dishes`
 --
 ALTER TABLE `dishes`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `restaurant_id` (`restaurant_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `photos`
@@ -161,6 +242,14 @@ ALTER TABLE `photos`
 --
 ALTER TABLE `restaurants`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `restaurants_dishes`
+--
+ALTER TABLE `restaurants_dishes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `restaurant_id` (`restaurant_id`),
+  ADD KEY `fk_restaurant_dishes_dishes` (`dishes_id`);
 
 --
 -- Indexes for table `reviews`
@@ -194,7 +283,7 @@ ALTER TABLE `user_dishes`
 -- AUTO_INCREMENT for table `dishes`
 --
 ALTER TABLE `dishes`
-  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
 
 --
 -- AUTO_INCREMENT for table `photos`
@@ -209,6 +298,12 @@ ALTER TABLE `restaurants`
   MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
+-- AUTO_INCREMENT for table `restaurants_dishes`
+--
+ALTER TABLE `restaurants_dishes`
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `reviews`
 --
 ALTER TABLE `reviews`
@@ -218,7 +313,7 @@ ALTER TABLE `reviews`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `user_dishes`
@@ -231,31 +326,32 @@ ALTER TABLE `user_dishes`
 --
 
 --
--- Constraints for table `dishes`
---
-ALTER TABLE `dishes`
-  ADD CONSTRAINT `dishes_ibfk_1` FOREIGN KEY (`restaurant_id`) REFERENCES `restaurants` (`id`) ON DELETE CASCADE;
-
---
 -- Constraints for table `photos`
 --
 ALTER TABLE `photos`
   ADD CONSTRAINT `photos_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `photos_ibfk_2` FOREIGN KEY (`dish_id`) REFERENCES `dishes` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `photos_ibfk_2` FOREIGN KEY (`dish_id`) REFERENCES `restaurants_dishes` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `restaurants_dishes`
+--
+ALTER TABLE `restaurants_dishes`
+  ADD CONSTRAINT `fk_restaurant_dishes_dishes` FOREIGN KEY (`dishes_id`) REFERENCES `dishes` (`id`),
+  ADD CONSTRAINT `restaurants_dishes_ibfk_1` FOREIGN KEY (`restaurant_id`) REFERENCES `restaurants` (`id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `reviews`
 --
 ALTER TABLE `reviews`
   ADD CONSTRAINT `reviews_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `reviews_ibfk_2` FOREIGN KEY (`dish_id`) REFERENCES `dishes` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `reviews_ibfk_2` FOREIGN KEY (`dish_id`) REFERENCES `restaurants_dishes` (`id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `user_dishes`
 --
 ALTER TABLE `user_dishes`
   ADD CONSTRAINT `user_dishes_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `user_dishes_ibfk_2` FOREIGN KEY (`dish_id`) REFERENCES `dishes` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `user_dishes_ibfk_2` FOREIGN KEY (`dish_id`) REFERENCES `restaurants_dishes` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
